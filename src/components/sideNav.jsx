73232fdadb4a94c8d5db926dc620d5a03e1f1741
Tasneem <thalim.glam@gmail.com>
2024-02-24 09:32:15 -0800

    Feeling frustarted while  working on components

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -1,13 +1,46 @@
-import React from "react";
-import {AiOutlineMenu} from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineHome, AiOutlineMenu } from 'react-icons/ai';
 
 
 const sideNav = () => {
-  return {
-    <div>
-    <AiOutlineMenu /> 
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    // console.log('state change');
+  };
+
+  return (
+    < div >
+    <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden' />
+  {
+    nav ? (
+      <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hoover:scale-110 ease-in'>
+          <AiOutlineHome size={20} />
+          <span className="pl-4">Home</span>
+        </a>
+        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hoover:scale-110 ease-in'>
+          <AiOutlineHome size={20} />
+          <span className="pl-4">Work</span>
+        </a>
+        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hoover:scale-110 ease-in'>
+          <AiOutlineHome size={20} />
+          <span className="pl-4">Projects</span>
+        </a>
+        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hoover:scale-110 ease-in'>
+          <AiOutlineHome size={20} />
+          <span className="pl-4">Download Resume</span>
+        </a>
+        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hoover:scale-110 ease-in'>
+          <AiOutlineHome size={20} />
+          <span className="pl-4">Contact</span>
+        </a>
+      </div>
+    ) : (
+        <div></div>
+      )}
     </div >
-  }
+  );
 }
 
 export default sideNav;
